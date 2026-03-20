897421e65414f3dc70eb8902003077575d311d93
Benjamin Lobos <blobosle@purdue.edu>
2026-03-20 01:41:57 -0400

    feat: added ScreenCopy icon

--- zip.sh
+++ zip.sh
@@ -2,4 +2,5 @@ zip -r ScreenCopy.zip ScreenCopy \
     -x "ScreenCopy/.git/*" \
     "ScreenCopy/node_modules/*" \
     "ScreenCopy/build/*" \
-    "ScreenCopy/dist/*"
+    "ScreenCopy/dist/*" \
+    "ScreenCopy/release/*"
